bffe0ed2fe98c7469c35e37929bf9ff41d76a9a8
akashchatterjee3502 <akashchatterjee3502@gmail.com>
2023-08-06 20:25:19 +0530

    audience changes

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       authorizationParams={{
         redirect_uri: "https://homyz-real-estate-lemon.vercel.app",
       }}
-      audience="http://localhost:8000"
+      audience="https://homyz-real-estate-seven.vercel.app/"
       scope="openid profile email"
     >
       <App />

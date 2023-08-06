9adae274f1598c53e86adff73e4c7a1a9f349a70
akashchatterjee3502 <akashchatterjee3502@gmail.com>
2023-08-06 20:34:51 +0530

    auth0 change

--- server/config/auth0Config.js
+++ server/config/auth0Config.js
@@ -1,7 +1,7 @@
 import { auth } from "express-oauth2-jwt-bearer";
 
 const jwtCheck = auth({
-  audience: "http://localhost:8000",
+  audience: "https://homyz-real-estate-seven.vercel.app",
   issuerBaseURL: "https://akash-chatterjee.us.auth0.com",
   tokenSigningAlg: "RS256",
 });
